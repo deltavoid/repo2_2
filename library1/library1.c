1cd1e7d7b83dd857a9cccc088237a728e1c6fb03
root <zhangqianyu.sys@bytedance.com>
2021-12-31 18:23:43 +0800

    add repo2_2

---
+++ library1/library1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#include <repo2_2/library1/public/library1.h>
+
+
+
+void repo2_2_library1_display()
+{
+    printf("repo2_2_library1_display\n");
+
+}
---
+++ library1/public/library1.h
@@ -0,0 +1,10 @@
+#ifndef REPO2_2_LIBRARY1_H
+#define REPO2_2_LIBRARY1_H
+
+
+
+extern void repo2_2_library1_display();
+
+
+
+#endif // REPO2_2_LIBRARY1_H
